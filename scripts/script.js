e953e274c1fe803095285a5fc50667c1c8b907e3
Artem Morozov <deadtems@yandex.ru>
2022-11-14 22:48:56 +0500

    fix: review fixed

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,36 @@
+// Обозначение перменных
 const popup = document.querySelector(".popup");
 const popupToggle = document.querySelector(".profile__edit-button");
 const popupClose = document.querySelector(".popup__close-btn");
-const popupName = document.querySelector(".popup__input-name");
-const popupAbout = document.querySelector(".popup__input-about");
-const popupSubmit = document.querySelector(".popup__submit-btn");
+const popupName = document.querySelector("#popup-name");
+const popupAbout = document.querySelector("#popup-about");
 
 const profileName = document.querySelector(".profile__name");
 const profilePopup = document.querySelector(".profile__about");
 
+let popupForm = document.querySelector(".popup__form");
+
+// Открытие
 function openedPopup() {
   popup.classList.add("popup_opened");
   popupName.value = profileName.textContent;
   popupAbout.value = profilePopup.textContent;
 }
 
+// Закрытие
 function closedPopup() {
   popup.classList.remove("popup_opened");
 }
 
-let popupForm = document.querySelector(".popup__form");
-
-function formSubmitHandler(evt) {
+// Сохранение данных и закрытие
+function handlerSubmitForm(evt) {
   evt.preventDefault();
   profileName.textContent = popupName.value;
   profilePopup.textContent = popupAbout.value;
   closedPopup();
 }
 
-popupForm.addEventListener("submit", formSubmitHandler);
+// Отслеживание действий
+popupForm.addEventListener("submit", handlerSubmitForm);
 popupClose.addEventListener("click", closedPopup);
 popupToggle.addEventListener("click", openedPopup);
